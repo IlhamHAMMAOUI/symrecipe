5fdc6cd3fbd89eef4037f88af103f2e1bec58897
Ilham HAMMAOUI <ilham3hammaoui@gmail.com>
2023-11-10 22:46:31 +0100

    Apprentissage de symfony1

--- src/Controller/IngredientController.php
+++ src/Controller/IngredientController.php
@@ -1,17 +1,27 @@
 <?php
 
 namespace App\Controller;
-
+use App\Form\IngredientType;
+use App\Entity\Ingredient;
 use App\Repository\IngredientRepository;
-use Symfony\Component\HttpFoundation\Request; // Assurez-vous d'importer correctement la classe Request
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request; // Assurez-vous d'importer correctement la classe Request
 
 class IngredientController extends AbstractController
 {
-    #[Route('/ingredient', name: 'app_ingredient')]
+    /**
+     * This function display all ingredients
+     *
+     * @param IngredientRepository $repository
+     * @param PaginatorInterface $paginator
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/ingredient', name: 'app_ingredient', methods:['GET'])]
     public function index(IngredientRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
         $ingredients = $paginator->paginate(
@@ -25,6 +35,39 @@ class IngredientController extends AbstractController
         return $this->render('pages/ingredient/index.html.twig', [
             // 'controller_name' => 'IngredientController',
             'ingredients'=> $ingredients
+        ]);
+    }
+    #[Route('/ingredient/nouveau', 'ingredient.new', methods:['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $manager):Response
+    {
+        $ingredient = new Ingredient();
+        // $task->setTask('Write a blog post');
+        // $task->setDueDate(new \DateTimeImmutable('tomorrow'));
+
+        $form = $this->createForm(IngredientType::class, $ingredient);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // $form->getData() holds the submitted values
+            // but, the original `$task` variable has also been updated
+            // $task = $form->getData();
+            // dd($form->getData());
+            $ingredient=$form->getData();
+            // dd($ingredient);
+            $manager->persist($ingredient);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'Votre ingrédient a été créé avec succés !'
+            );
+            // ... perform some action, such as saving the task to the database
+
+            // return $this->redirectToRoute('ingredient.index');
+        }
+
+
+        return $this->render('pages/ingredient/new.html.twig', [
+            'form'=>$form->createView()
         ]);
     }
 }
